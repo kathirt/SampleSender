3f02b3d5faa02c891b7e9d9c359deecd78969dc0
cholasm <kathanga@microsoft.com>
2020-06-10 12:47:17 -0700

    Fixed the HTTP Post issue in the code

--- Program.cs
+++ Program.cs
@@ -9,12 +9,16 @@ namespace SampleSender
     using System.Threading.Tasks;
     using Microsoft.Azure.EventHubs;
     using Newtonsoft.Json;
+    using System.Web;
+    using System.Globalization;
+    using System.Security.Cryptography;
 
     public class Program
     {
         private static EventHubClient eventHubClient;
         private const string EventHubConnectionString = "Endpoint=sb://ehdriverinfo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ATgAlpSCvmSwR0S0M+rO/Y6iy4OJo8ycD6B3jMbct6E=";
         private const string EventHubName = "deviceinfo";
+        private const string TestEventHubName = "eventhubpost";
         private static bool SetRandomPartitionKey = false;
 
         public static void Main(string[] args)
@@ -23,110 +27,90 @@ namespace SampleSender
         }
 
         private static async Task MainAsync(string[] args)
-        {
-            // Creates an EventHubsConnectionStringBuilder object from a the connection string, and sets the EntityPath.
-            // Typically the connection string should have the Entity Path in it, but for the sake of this simple scenario
-            // we are using the connection string from the namespace.
-            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EventHubConnectionString)
-            {
-                EntityPath = EventHubName
-            };
+        {        
 
-            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
-
-            await PostTelemetryAsync(10);
-
-            //await SendMessagesToEventHub(100);
-
-            //await eventHubClient.CloseAsync();
-
+            await SendDataUsingHttp();
             Console.WriteLine("Press any key to exit.");
             Console.ReadLine();
         }
 
-        // Creates an Event Hub client and sends 100 messages to the event hub.
-        private static async Task SendMessagesToEventHub(int numMessagesToSend)
+        /// <summary>
+        /// Push data into event hub through Http POST
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<HttpResponseMessage> SendDataUsingHttp()
         {
-            //var rnd = new Random();
-
-            for (var i = 0; i < numMessagesToSend; i++)
-            {
-                try
-                {
-                    var message = $"Message {i}";
-
-                    // Set random partition key?
-                    if (SetRandomPartitionKey)
-                    {
-                        var pKey = Guid.NewGuid().ToString();
-                        await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)), pKey);
-                        Console.WriteLine($"Sent message: '{message}' Partition Key: '{pKey}'");
-                    }
-                    else
-                    {
-                        await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
-                        Console.WriteLine($"Sent message: '{message}'");
-                    }
-                }
-                catch (Exception exception)
-                {
-                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
-                }
-
-                await Task.Delay(10);
-            }
-
-            Console.WriteLine($"{numMessagesToSend} messages sent.");
-        }
-
-        private static Task<HttpResponseMessage> PostTelemetryAsync(int count)
-        {
-            // Use Event Hubs Signature Generator 0.2.0.1 to generate the token
-            var sas = "SharedAccessSignature sr=ATgAlpSCvmSwR0S0M+rO/Y6iy4OJo8ycD6B3jMbct6E=";
 
             // Namespace info.
-            var serviceNamespace = "ehDriverInfo";
-            var hubName = "deviceinfo";
-            var url = string.Format("{0}/publishers/{1}/messages", hubName, 1001);
-            Task<HttpResponseMessage> returnTask = null;
+            var serviceNamespace = "ehdriverinfo";
+            var hubName = "eventhubpost";
+            var url = string.Format("{0}/publishers/{1}/messages", hubName, 1);
+            //var url = string.Format("{0}/messages", hubName);
+            var baseUri = new
+             Uri(string.Format("https://{0}.servicebus.windows.net/"
+                          , serviceNamespace));
+            var SharedAccessKeyName = "RootManageSharedAccessKey";
+            var SharedAccessKey = "ATgAlpSCvmSwR0S0M+rO/Y6iy4OJo8ycD6B3jMbct6E=";
+            var sasToken =
+            createSasToken(baseUri.ToString(), SharedAccessKeyName,
+                           SharedAccessKey);
+            var evtData = new
+            {
+                Temperature = new Random().Next(20, 50)
+            };
 
+            var payload = JsonConvert.SerializeObject(evtData);
             // Create client.
             var httpClient = new HttpClient
             {
-                BaseAddress = new Uri(string.Format("https://{0}.servicebus.windows.net/", serviceNamespace))
+                BaseAddress = baseUri
             };
 
-            for (int i = 0; i < count; i++)
+
+            httpClient.DefaultRequestHeaders.Accept.Clear();     
+            bool isAuthAdded = httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", sasToken);
+
+            var content = new StringContent(payload, Encoding.UTF8);
+            content.Headers.ContentType = new
+              System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            HttpResponseMessage result = null;
+            try
             {
-                //var messageInfo = new { id = i, temperature = 70 };                
-                var messageInfo = new
-                {
-                    v = 1,
-                    t = "event",
-                     cid = "C9A57091-81E1-4FDB-BBFC-D10D29ACE142",
-                    aip = true,
-                    tid = "UA-41423377-13",
-                    ul = "en",
-                    av = "6.3.37b4",
-                    an = "Driver",
-                    ec = "HardwareActions",
-                    ea = "Use transducer tip",
-                    ev = 3,
-                    el = "5C~1063A0",
-                    cd1 = "Cintiq%20Pro%2032%20%28DTH-3220%29"
-                };
-
-                var payload = JsonConvert.SerializeObject(messageInfo);
-
-                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", sas);
-
-                var content = new StringContent(payload, Encoding.UTF8, "application/json");
-
-                //content.Headers.Add("ContentType", DeviceTelemetry.ContentType);
-
-                returnTask = httpClient.PostAsync(url, content);
+                result = await httpClient.PostAsync(url, content);
+                Console.WriteLine(result.StatusCode);
             }
-            return returnTask;
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return result;
+        }
+
+       
+        /// <summary>
+        /// Create SAS token in the format required by EH
+        /// </summary>
+        /// <param name="baseUri"></param>
+        /// <param name="keyName"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private static string createSasToken(string baseUri, string keyName, string key)
+        {
+            TimeSpan sinceEpoch = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            var week = 60 * 60 * 24 * 7;
+            var expiration = Convert.ToString((int)sinceEpoch.TotalSeconds +
+            week);
+            string stringToSign = HttpUtility.UrlEncode(baseUri) + "\n" +
+            expiration;
+            HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(key)); //--
+            var signature =
+            Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(
+            stringToSign)));
+            var sasToken = String.Format(CultureInfo.InvariantCulture,
+                   "SharedAccessSignature sr={0}&sig={1}&se={2}&skn={3}",
+            HttpUtility.UrlEncode(baseUri), HttpUtility.UrlEncode(signature),
+            expiration, keyName);
+            return sasToken;
         }
 
     }
